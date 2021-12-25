0732308012dd5df863db02a2856176604dee191f
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-12-25 22:04:17 +0100

    [JRip] prediccion como cuatro clases distintas, en lugar de dos predicciones por separado

--- models/JRip.R
+++ models/JRip.R
@@ -9,7 +9,7 @@ library(caret)
 
 datos <- leer_datos("../data/training_set_features_preprocessed.csv", 
 					"../data/training_set_labels.csv", 
-					"../data/test_set_features_preprocessed.csv")
+					"../data/test_set_features_preprocessed.csv", juntar_etiquetas = TRUE)
 
 training <- datos[[1]]
 training_labels <- datos[[2]]
@@ -29,15 +29,13 @@ test <- datos[[3]]
 train_JRip <- function(formula, datos, num_cv = 10, tune_grid = NULL) {
 	
 	# creamos el control de training con validación cruzada
-	fit_control <- trainControl(method="cv", number = num_cv, verboseIter = T,
-								classProbs = TRUE,
-								summaryFunction = twoClassSummary)
+	fit_control <- trainControl(method="cv", number = num_cv, verboseIter = T)
 
 	# entrenamos el modelo
 	modelo_JRip_entrenado <- train(formula, 
 								  data = datos,
 								  method = "JRip", 
-								  metric = "ROC",
+								  metric = "Accuracy",
 								  trControl = fit_control,
 								  tuneGrid = tune_grid)
 	
@@ -104,6 +102,8 @@ levels(training_completo$seasonal_vaccine) <- c("X0", "X1")
 tune_grid_seasonal_completo <- expand.grid(NumOpt = c(3), NumFolds = c(3), MinWeights = c(3))
 modelo_JRip_seasonal_completo <- train_JRip(seasonal_vaccine ~ ., training_completo, num_cv = 10, tune_grid = tune_grid_seasonal_completo)
 
+summary(modelo_JRip_seasonal_completo)
+
 test_con_predicciones_h1n1 <- base::cbind(test, h1n1_vaccine = ifelse(predicciones_h1n1_vaccine_test$`X1` < 0.5, 0, 1))
 test_con_predicciones_h1n1$h1n1_vaccine <- as.factor(test_con_predicciones_h1n1$h1n1_vaccine)
 levels(test_con_predicciones_h1n1$h1n1_vaccine) <- c("X0", "X1")
@@ -118,3 +118,38 @@ resultados_predicciones_completo <- data.frame(respondent_id = c(26707:53414),
 
 #Lo exportamos a CSV
 write.csv(resultados_predicciones_completo, "../results/JRip_metric_ROC_h1n1_predictor_results.csv", row.names = F) 
+
+
+
+
+
+
+datos_unica_etiqueta <- leer_datos("../data/training_set_features_preprocessed.csv", 
+					"../data/training_set_labels.csv", 
+					"../data/test_set_features_preprocessed.csv", juntar_etiquetas = TRUE)
+
+training <- datos_unica_etiqueta[[1]]
+training_labels <- datos_unica_etiqueta[[2]]
+
+test <- datos_unica_etiqueta[[3]]
+
+
+# probamos a predecir ambas variables juntas
+
+training_con_etiqueta <- base::cbind(training, training_labels[,1])
+
+# tune grid añadido a posteriori una vez tenemos los mejores hiperparámetros
+tune_grid_completo <- expand.grid(NumOpt = c(3), NumFolds = c(2), MinWeights = c(2))
+modelo_JRip_completo <- train_JRip(Y ~ ., training_con_etiqueta, num_cv = 10, tune_grid = tune_grid_completo)
+
+summary(modelo_JRip_completo)
+
+predicciones_test <- predict(modelo_JRip_completo, test, type = "prob")
+
+
+resultados_predicciones_completo <- data.frame(respondent_id = c(26707:53414), 
+												   h1n1_vaccine = predicciones_test$`10` + predicciones_test$`11`, 
+												   seasonal_vaccine = predicciones_test$`01` + predicciones_test$`11`)
+
+write.csv(resultados_predicciones_completo, "../results/JRip_cuatro_clases_results.csv", row.names = F) 
+
